e4221e705f16648bf0ce804744de3b9c407c4744
zdyangGaven <yang1986jl@163.com>
2019-07-05 09:48:06 +0800

    第一次提交

---
+++ csj/src/main/java/com/nsoft/csj/CsjDemoApplication.java
@@ -0,0 +1,15 @@
+package com.nsoft.csj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsjDemoApplication {
+
+    
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsjDemoApplication.class, args);
+    }
+
+}
